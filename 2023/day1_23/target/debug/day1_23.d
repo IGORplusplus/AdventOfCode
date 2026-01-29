09b27c9f23bc36edec81243c6cccac70e9260440
Igor Leeck <igorleeck@gmail.com>
2026-01-29 02:12:27 -0600

    removed executables and bloat hopefully

--- 2023/day1_23/target/debug/day1_23.d
+++
@@ -1,1 +0,0 @@
-/home/igorl/AdventofCode/2023/day1_23/target/debug/day1_23: /home/igorl/AdventofCode/2023/day1_23/src/main.rs
--- 2023/day1_23/target/debug/deps/day1_23-f0da2dad37a92ac8.d
+++
@@ -1,5 +0,0 @@
-/home/igorl/AdventofCode/2023/day1_23/target/debug/deps/libday1_23-f0da2dad37a92ac8.rmeta: src/main.rs
-
-/home/igorl/AdventofCode/2023/day1_23/target/debug/deps/day1_23-f0da2dad37a92ac8.d: src/main.rs
-
-src/main.rs:
